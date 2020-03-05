738332af79244366750a770a6ac2390288ef923c
= <=>
2020-03-05 15:59:20 -0600

    Parcial I

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-document.addEventListener("DOMContentLoaded", e=>{
-    document.querySelector("#frmAlumnos").addEventListener("submit", event=>{
-        event.preventDefault();
+
+
+function monedas(){
+    
+    let de = document.getElementById("cboDe").Value,
+    a = document.getElementById("cboA").Value,
+    cantidad = document.getElementById("txtCantidadConversores").Value,
+     res = document.getElementById("lblRespuesta").Value;
+
+     let monedas = {
+     'dolar':1,
+     'colones':8.75,
+     'yenes':111.27,
+     'rupia':69.75,
+     'lempiras':24.36,
+     'peso mx':19.36,
+     'bitcoin':0.00026,
 
 
 
+   };
 
-    });
-});
+   
+   res.innerHTML = `Respuesta: ${ monedas[a] / monedas[de] * cantidad }`;
+
+}
 
 // JOSUE ISAAC APARICIO DÍAZ USIS028518
 
